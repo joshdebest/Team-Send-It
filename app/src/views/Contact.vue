5e0bbeca7be7c7439e8b72762865b414d485b860
Joshua DeBest <joshdebest@Joshuas-MacBook-Pro.local>
2018-10-16 19:20:55 -0700

    test

---
+++ app/src/views/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="contact">
+    <h2 class="title">Contact form</h2> 
+      <fieldset id="name-yui_3_17_2_52_1499543463106_2817" class="form-item fields name required">
+              <div class="title">Name <span class="required">*</span></div>
+              <legend>Name</legend>
+              
+                <div class="field first-name">
+                  <label class="caption"><input class="field-element field-control" name="fname"
+                  x-autocompletetype="given-name" type="text"
+                  spellcheck="false"
+                  maxlength="30"
+                  data-title="First" />
+                  First Name</label>
+                </div>
+                <div class="field last-name">
+                  <label class="caption"><input class="field-element field-control" name="lname"
+                  x-autocompletetype="surname" type="text"
+                  spellcheck="false" maxlength="30" data-title="Last" />
+                  Last Name</label>
+                </div>
+            </fieldset>
+  </div>
+</template>
