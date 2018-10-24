4c78b83a5d2b0192394860679d185b257a19a035
Joshua DeBest <joshdebest@Joshuas-MacBook-Pro.local>
2018-10-23 20:12:37 -0700

    services & repairs

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>  |
       <router-link to="/accessories">Accessories</router-link>  |
+      <router-link to="/services_&_repairs">Services & Repairs</router-link>  |
       <router-link to="/contact">Contact</router-link>  
       
     </div>
