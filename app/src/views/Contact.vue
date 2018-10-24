9c0a9952322822b0d0e5e1d6d25faa7ae6aa355c
Joshua DeBest <joshdebest@Joshuas-MacBook-Pro.local>
2018-10-23 19:02:21 -0700

    contact page

--- app/src/views/Contact.vue
+++ app/src/views/Contact.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="contact">
-    <h2 class="title">Contact form</h2> 
-      <fieldset id="name-yui_3_17_2_52_1499543463106_2817" class="form-item fields name required">
-              <div class="title">Name <span class="required">*</span></div>
-              <legend>Name</legend>
-              
-                <div class="field first-name">
-                  <label class="caption"><input class="field-element field-control" name="fname"
-                  x-autocompletetype="given-name" type="text"
-                  spellcheck="false"
-                  maxlength="30"
-                  data-title="First" />
-                  First Name</label>
-                </div>
-                <div class="field last-name">
-                  <label class="caption"><input class="field-element field-control" name="lname"
-                  x-autocompletetype="surname" type="text"
-                  spellcheck="false" maxlength="30" data-title="Last" />
-                  Last Name</label>
-                </div>
-            </fieldset>
+    <h2 class="title">Contact Us</h2> 
+      <div id="after_submit"></div>
+      <form id="contact_form" action="#" method="POST" enctype="multipart/form-data">
+        <div class="row">
+          <label class="required" for="name">Name:</label><br />
+          <input id="name" class="input" name="name" type="text" value="" size="30" /><br />
+          <span id="name_validation" class="error_message"></span>
+        </div>
+        <div class="row">
+          <label class="required" for="email">Email:</label><br />
+          <input id="email" class="input" name="email" type="text" value="" size="30" /><br />
+          <span id="email_validation" class="error_message"></span>
+        </div>
+        <div class="row">
+          <label class="required" for="phone">Phone Number:</label><br />
+          <input id="phone" class="input" name="phone" type="text" value="" size="30" /><br />
+          <span id="phone_validation" class="error_message"></span>
+        </div>
+        <div class="row">
+          <label class="required" for="message">Message:</label><br />
+          <textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
+          <span id="message_validation" class="error_message"></span>
+        </div>  
+          <input id="submit_button" type="submit" value="Submit" />
+      </form>
   </div>
 </template>
