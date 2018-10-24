cb42db43923c8e339330246771f3e2110affd161
Lucas Han <lhan@Lucass-MacBook-Pro.local>
2018-10-23 19:37:47 -0700

    store info ui

--- app/src/views/Contact.vue
+++ app/src/views/Contact.vue
@@ -1,29 +1,48 @@
 <template>
   <div class="contact">
-    <h2 class="title">Contact Us</h2> 
+
+    <h2 class="title">Contact Us</h2>
       <div id="after_submit"></div>
       <form id="contact_form" action="#" method="POST" enctype="multipart/form-data">
-        <div class="row">
+
+        <div class="row" >
           <label class="required" for="name">Name:</label><br />
           <input id="name" class="input" name="name" type="text" value="" size="30" /><br />
           <span id="name_validation" class="error_message"></span>
         </div>
-        <div class="row">
+
+        <div class="row" >
           <label class="required" for="email">Email:</label><br />
           <input id="email" class="input" name="email" type="text" value="" size="30" /><br />
           <span id="email_validation" class="error_message"></span>
         </div>
+
         <div class="row">
           <label class="required" for="phone">Phone Number:</label><br />
           <input id="phone" class="input" name="phone" type="text" value="" size="30" /><br />
           <span id="phone_validation" class="error_message"></span>
         </div>
+
         <div class="row">
           <label class="required" for="message">Message:</label><br />
           <textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
           <span id="message_validation" class="error_message"></span>
-        </div>  
-          <input id="submit_button" type="submit" value="Submit" />
-      </form>
+        </div>
+
+        <div class="button">
+          <input id="submit_button" type="submit" value="Submit" align="left"/>
+        </div>
+    </form>
+
+    <h3 class="phone">Phone: 123-123-1234</h3>
+
+    <h4 class="hours" style="white-space: pre-wrap;"><strong>Hours</strong></h4>
+      <p style="white-space: pre-wrap;">Monday to Friday 10 AM - 6 PM<br>Saturday 10 AM - 5 PM<br>Sunday 12 PM - 5 PM</p>
+      <p style="white-space: pre-wrap;"><strong>*We will be closed on all national holidays</strong></p>
+
+    <h5 style="white-space: pre-wrap;"><strong>Location</strong></h5>
+    <p style="white-space: pre-wrap;"><a href="https://www.google.com/maps/place/Foothill+Cyclery/@35.293542,-120.6753547,17z/data=!3m1!4b1!4m5!3m4!1s0x80ecf051fdb9435b:0xb27885a863b62dae!8m2!3d35.293542!4d-120.673166"
+                                         target="_blank"><strong>767 East Foothill Blvd Ste B<br>San Luis Obispo, Ca 93405</strong></a>
+      <br><br>We are located in the Foothill Plaza Shopping Center right next door to California Fresh Market.</p>
   </div>
 </template>
