4a0dd8ca6e3754b91f52d85cb4b92d6d2169aae6
Lucas Han <lhan@Lucass-MacBook-Pro.local>
2018-10-25 19:38:44 -0700

    log in ui

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,47 +1,101 @@
 <template>
-  <div id="login">
-    <h1>Login</h1>
-    <input type="text" name="username" v-model="input.username" placeholder="Username" />
-    <input type="password" name="password" v-model="input.password" placeholder="Password" />
-    <button type="button" v-on:click="login()">Login</button>
+  <div class="content has-text-centered">
+    <h1 class="is-title is-bold">Admin Login</h1>
+
+    <div class="columns is-vcentered">
+      <div class="column is-6 is-offset-3">
+        <div class="box">
+          <div v-show="error" style="color:red; word-wrap:break-word;">{{ error }}</div>
+          <form v-on:submit.prevent="login">
+            <label class="label">Email</label>
+            <p class="control">
+              <input v-model="data.body.username" class="input" type="text" placeholder="email@example.org">
+            </p>
+            <label class="label">Password</label>
+            <p class="control">
+              <input v-model="data.body.password" class="input" type="password" placeholder="password">
+            </p>
+
+            <p class="control">
+              <label class="checkbox">
+                <input type="checkbox" v-model="data.rememberMe">
+                Remember me
+              </label>
+            </p>
+            <p class="control">
+              <button type="submit" class="button is-primary">Login</button>
+              <button class="button is-default">Cancel</button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-        name: 'Login',
-        data() {
+        data () {
             return {
-                input: {
-                    username: "",
-                    password: ""
-                }
+                data: {
+                    body: {
+                        username: null,
+                        password: null
+                    },
+                    rememberMe: false
+                },
+                error: null
             }
         },
+        mounted () {
+            if (this.$auth.redirect()) {
+                console.log('Redirect from: ' + this.$auth.redirect().from.name)
+            }
+            // Can set query parameter here for auth redirect or just do it silently in login redirect.
+        },
         methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "secure" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
+            login () {
+                var redirect = this.$auth.redirect()
+                this.$auth.login({
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: this.data.body,
+                    rememberMe: this.data.rememberMe,
+                    redirect: {name: redirect ? redirect.from.name : 'Home'},
+                    success (res) {
+                        console.log('Auth Success')
+                        // console.log('Token: ' + this.$auth.token())
+                        // console.log(res)
+                    },
+                    error (err) {
+                        if (err.response) {
+                            // The request was made, but the server responded with a status code
+                            // that falls out of the range of 2xx
+                            // console.log(err.response.status)
+                            // console.log(err.response.data)
+                            // console.log(err.response.headers)
+                            this.error = err.response.data
+                        } else {
+                            // Something happened in setting up the request that triggered an Error
+                            console.log('Error', err.message)
+                        }
+                        console.log(err.config)
                     }
-                } else {
-                    console.log("A username and password must be present");
-                }
+                })
             }
         }
+        // filters: {
+        //   json: function (value) {
+        //     console.log(value)
+        //     return value
+        //   }
+        // }
     }
 </script>
 
-<style scoped>
-  #login {
-    width: 500px;
-    border: 1px solid #CCCCCC;
-    background-color: #FFFFFF;
-    margin: auto;
-    margin-top: 200px;
-    padding: 20px;
+<style lang="scss" scoped>
+  .is-title {
+    text-transform: capitalize;
   }
 </style>
