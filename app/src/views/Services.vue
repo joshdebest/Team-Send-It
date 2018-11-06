22877f4f50de82e3293573a1e86eda58b12181cc
Joshua DeBest <joshdebest3@gmail.com>
2018-11-05 19:55:39 -0800

    updated some pages

--- app/src/views/Services.vue
+++ app/src/views/Services.vue
@@ -3,23 +3,29 @@
     <h2 class="title">Service & Repairs</h2>
       <div id="after_submit"></div>
       <form id="contact_form" action="#" method="POST" enctype="multipart/form-data">
-    </form>
-    <img src = http://sidehustlehq.com/wp-content/uploads/2017/07/bicyclerepair.jpg>
-    <img src = http://www.ridesonair.com/images/full-sized/professional-bike-workshop.jpg>
+      </form>
+    <img src = "http://sidehustlehq.com/wp-content/uploads/2017/07/bicyclerepair.jpg"
+        style = "width: 450px; height: 300px;">
+    <img src = "http://www.ridesonair.com/images/full-sized/professional-bike-workshop.jpg"
+        style = "width: 450px; height: 300px;">
     <h3 class="phone">PICKUP AND DELIVERY AVAILABLE! - CALL STORE FOR DETAILS!</h3>
-    <p style="white-space: pre-wrap;"><strong>SERVICE PACKAGES</strong><br>Minor tune-up (lube, air tires, check brakes, check derailleurs, check components are tight)<br>Major tune-up<br>Drivetrain overhaul<br>Brake overhaul<br>Complete bicycle overhaul<br>Complete bearing overhaul<br>Electric bike service<br>Bosch eBike drivetrain service<br>Detail bike: thoroughly clean bicycle and components; polish and wax bicycle</p>
-    <p style="white-space: pre-wrap;"><strong>BRAKES</strong><br>Replace brake cable(s)<br>Replace brake shoes<br>Minor brake adjustment<br>Major brake adjustment<br>Bleed hydraulic disc brakes<br>Disc-brake rotor repair<br>Hydraulic disc brake line repair/replace<br>Install and adjust new brakes</p>   
-    <p style="white-space: pre-wrap;"><strong>WHEELS</strong><br>Flat tire repair<br>Replace tire(s)<br>Replace broken spoke(s)<br>Install new front wheel<br>Install new rear wheel<br>Wheel truing (Minor/Major)<br>Wheel retensioning, dishing and truing<br>Custom wheel building<br>Hub bearing overhaul</p>
-    <h3 class="phone">Phone: 123-123-1234</h3>
-
-    <h4 class="hours" style="white-space: pre-wrap;"><strong>Hours</strong></h4>
-      <p style="white-space: pre-wrap;">Monday to Friday 10 AM - 6 PM<br>Saturday 10 AM - 5 PM<br>Sunday 12 PM - 5 PM</p>
-      <p style="white-space: pre-wrap;"><strong>*We will be closed on all national holidays</strong></p>
-
-    <h5 style="white-space: pre-wrap;"><strong>Location</strong></h5>
-    <p style="white-space: pre-wrap;"><a href="https://www.google.com/maps/place/Foothill+Cyclery/@35.293542,-120.6753547,17z/data=!3m1!4b1!4m5!3m4!1s0x80ecf051fdb9435b:0xb27885a863b62dae!8m2!3d35.293542!4d-120.673166"
-                                         target="_blank"><strong>767 East Foothill Blvd Ste B<br>San Luis Obispo, Ca 93405</strong></a>
-      <br><br>We are located in the Foothill Plaza Shopping Center right next door to California Fresh Market.</p>
-      </body>
+    <br>
+    <h3 class="phone">List of services below:</h3>
+    <div class="services_paragraphs" >
+      <div class="service_texts" style = margin:0px;>
+          <p class="text-center"><strong>SERVICE PACKAGES</strong><br>Minor tune-up<br>Major tune-up<br>Drivetrain overhaul<br>Brake overhaul<br>Complete bicycle overhaul<br>Complete bearing overhaul<br>Electric bike service<br>Bosch eBike drivetrain service<br>Detail bike: thoroughly clean bicycle and components; polish and wax bicycle</p>
+          <p class="text-center"><strong>BRAKES</strong><br>Replace brake cable(s)<br>Replace brake shoes<br>Minor brake adjustment<br>Major brake adjustment<br>Bleed hydraulic disc brakes<br>Disc-brake rotor repair<br>Hydraulic disc brake line repair/replace<br>Install and adjust new brakes</p>
+      </div>
+      <div class="service_texts" style = margin-left:0px;>
+          <p style="white-space: pre-wrap;"><strong>FRONT END</strong><br>Adjust headset<br>Overhaul headset<br>Replace headset<br>Replace handlebars<br>Replace stem<br>Replace handlebar tape<br>Replace handlebar grips</p>
+          <p style="white-space: pre-wrap;"><strong>FRAME &amp; FORK</strong><br>Check/align dropouts<br>Replace fork<br>Suspension fork overhaul<br>Rear shock air sleeve<br>Frame preparation for assembly (thread chase &amp; face)<br>Derailleur hanger repair/replacement</p>
+      </div>    
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+  #services_paragraphs{
+    font-size: 300%;
+  }
+</style>
