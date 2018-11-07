e648764bad4011ddb8bd0e82ed37eb62d1a707b7
haydenbaker <hkbaker@calpoly.edu>
2018-11-06 20:09:25 -0800

    added tests

--- app/tests/e2e/plugins/index.js
+++ app/tests/e2e/plugins/index.js
@@ -3,7 +3,6 @@ const webpack = require('@cypress/webpack-preprocessor');
 
 module.exports = (on, config) => {
   on('file:preprocessor', webpack({
-    webpackOptions: require('@vue/cli-service/webpack.config'),
     watchOptions: {},
   }));
 
